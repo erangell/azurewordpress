d5aa43353ed6ecfb1a72e62df34f4a888b9a4a2b
bartr <bartr@outlook.com>
2017-05-16 20:49:30 -0700

    readme updates

--- sql/create.sql
+++ sql/create.sql
@@ -2,7 +2,7 @@ drop database if exists wordpress;
 
 create database wordpress;
 
-create user 'wordpress' IDENTIFIED BY "Azure2017"; 
+create user 'wordpress' IDENTIFIED BY "WP-Passw0rd"; 
 
 GRANT ALL PRIVILEGES ON wordpress.* TO 'wordpress'; 
 FLUSH PRIVILEGES;
