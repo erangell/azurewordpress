487669c3248db94eb87f05778ee05cb85c602851
bartr <bartr@outlook.com>
2017-05-17 00:53:32 +0000

    wprun cleanup

--- docker/wprun.sh
+++ docker/wprun.sh
@@ -1,13 +1,11 @@
+# git the latest code
 pushd .
 cd /var/www
 git pull
 popd
 
-if test -f "$APACHE_ENVVARS"; then
-        . "$APACHE_ENVVARS"
-fi
-
 # Apache gets grumpy about PID files pre-existing
 rm -f "$APACHE_PID_FILE"
 
-exec apache2 -DFOREGROUND "$@"
+# Run Apache in the foreground
+exec apache2 -DFOREGROUND
