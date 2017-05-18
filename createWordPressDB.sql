3ca1e7a7761963e7b7d207ec102d6000029e0f78
bartr <bartr@outlook.com>
2017-05-17 17:39:29 -0700

    dockerfile / sql update

--- createWordPressDB.sql
+++ createWordPressDB.sql
@@ -1,9 +1,5 @@
 /* drop database if exists wordpress; */
 
 create database wordpress;
-
-create user 'wordpress' IDENTIFIED BY "WP-Passw0rd"; 
-
-GRANT ALL PRIVILEGES ON wordpress.* TO 'wordpress'; 
+GRANT ALL PRIVILEGES ON wordpress.* TO 'wordpress' IDENTIFIED BY 'WP-Passw0rd';
 FLUSH PRIVILEGES;
-
