206ee34a9cee82e802fcb1a5cb9412611e547323
bartr <bartr@outlook.com>
2017-05-19 01:21:23 +0000

    wprun SSL update support

--- docker/wprun.sh
+++ docker/wprun.sh
@@ -1,3 +1,15 @@
+# save the SSL CRT if specified
+if [[ $SSL_CRT ]];
+then
+  echo "$SSL_CRT" > /etc/ssl/apache.crt;
+fi
+
+# save the SSL Key if specified
+if [[ $SSL_KEY ]];
+ then
+  echo "$SSL_KEY" > /etc/ssl/apache.key;
+fi
+
 # git the latest code
 git clone "$GIT_REPO" /var/www
 
